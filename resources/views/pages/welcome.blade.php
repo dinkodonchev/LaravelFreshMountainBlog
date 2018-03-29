9b3159378a33c97b00f68bc2fa022f31f258add8
vagrant <vagrant@homestead>
2018-03-29 19:48:21 +0000

    adde main and introduced sections

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,66 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('main')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <title>Dinyo's Mountain Blog</title>
-  </head>
-  <body>
-
-    <!-- BOOTSTRAP NAVBAR -->
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-    <h1>The Road goes ever on and on...</h1>
-
-    <img src="{{ asset('images/Kalinite.jpg')}}">
-
-    <div class="container">
+@section('content')
       <div class="row">
         <div class="col-md-12">
             <div class="jumbotron">
@@ -74,7 +14,7 @@
     
 
     <div class="row">
-      <div class="col-md-8" style="background-color: orange">
+      <div class="col-md-8" style="">
         <div class="post">
           <h3>Post title</h3>
         <p>Lorem ipsum dolor sit amet, tempor non inceptos, erat architecto accumsan, augue amet interdum esse lacus risus. Commodo eros felis parturient ac, mauris elementum posuere quis sit eros lacinia. Ut in in urna etiam, mauris nec amet, quisque pede et tincidunt, mattis donec dui, montes libero habitasse ac in dictum. Sollicitudin non enim feugiat velit a, lobortis senectus aliquam sed volutpat malesuada. Tortor vivamus cras, curabitur suspendisse, malesuada interdum lacus maecenas, lacinia sit, adipiscing suspendisse torquent in.</p> 
@@ -102,20 +42,11 @@
       </div>
 
 
-      <div class="col-md-3 col-md-offset-1" style="background-color: red">
+      <div class="col-md-3 col-md-offset-1" style="">
         <h2>Sidebar</h2>
         <p>Lorem ipsum dolor sit amet, tempor non inceptos, erat architecto accumsan, augue amet interdum esse lacus risus. Commodo eros felis parturient ac, mauris elementum posuere quis sit eros lacinia. Ut in in urna etiam, mauris nec amet, quisque pede et tincidunt, mattis donec dui, montes libero habitasse ac in dictum. Sollicitudin non enim feugiat velit a, lobortis senectus aliquam sed volutpat malesuada. Tortor vivamus cras, curabitur suspendisse, malesuada interdum lacus maecenas, lacinia sit, adipiscing suspendisse torquent in.</p>
 
       </div>
     </div>
 
-    </div>
-    <!-- End of container -->
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
