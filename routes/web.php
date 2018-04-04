c305b432735f9ab31ec1cd2d7e04c62927634aca
vagrant <vagrant@homestead>
2018-04-04 21:33:30 +0000

    added password resets routes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,12 @@
 
 Auth::routes();
 
+//Password reset routes
 
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 
 /*
